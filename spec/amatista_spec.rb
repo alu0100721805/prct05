c7cff0ede8bab539280b91957f2bf6db141e1dc4
Juan José Gregorio Díaz Marrero <alu0100721805@ull.edu.es>
2015-10-27 17:39:54 +0000

    Creando Segunda Expectativa - Acceso a atributos de la Clase Fraccion

--- spec/amatista_spec.rb
+++ spec/amatista_spec.rb
@@ -4,14 +4,21 @@
 describe Amatista do
  before :each do
 
-	@fraccion = Amatista::Fraccion.new(2,4) 
+	@fraccion1 = Amatista::Fraccion.new(2,4)
+        @fraccion2 = Amatista::Fraccion.new(1,2) 
   end
 
  describe " Expectativas Clase Fracción" do
 
 	it " Expectativa Instacia Fraccion " do
-	   expect(@fraccion).to be_truthy
+	   expect(@fraccion1).to be_truthy
 	end 
+        
+        it " Expectativa acceso atributos de la clase Fraccion" do 
+	    expect(@fraccion1).to be_instance_of(Amatista::Fraccion)
+            expect(@fraccion1).to respond_to(:numerador)
+	    expect(@fraccion1).to respond_to(:denominador)
+	end
  end
 
 end
