6a8afae9ade3daa831e995c48d2befaa53542212
Juan José Gregorio Díaz Marrero <alu0100721805@ull.edu.es>
2015-10-29 00:19:41 +0000

    Definiendo bloque Set-Up

--- test/tc_fraccion.rb
+++ test/tc_fraccion.rb
@@ -3,7 +3,8 @@ require 'lib/amatista.rb'
 
 class TestFraccion < Test::Unit::TestCase
   def setup
- 	
+ 	@fraccion1 = Amatista::Fraccion.new(2,4)
+	@fraccion2 = Amatista::Fraccion.new(1,4)
   end
   def tear_down
     # Acciones que se ejecutan despues de correr las pruebas
