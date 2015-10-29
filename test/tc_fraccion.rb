b0c1cbec534750dfe309f7b4a4f0ed552f3b34eb
Juan José Gregorio Díaz Marrero <alu0100721805@ull.edu.es>
2015-10-29 01:11:01 +0000

    Corriendo Pruebas Unitarias del Bloque Simple 'rake simple'

--- test/tc_fraccion.rb
+++ test/tc_fraccion.rb
@@ -5,12 +5,19 @@ class TestFraccion < Test::Unit::TestCase
   def setup
  	@fraccion1 = Amatista::Fraccion.new(2,4)
 	@fraccion2 = Amatista::Fraccion.new(1,4)
+        @fraccion3 = Amatista::Fraccion.new(8,4)
   end
   def tear_down
     # Acciones que se ejecutan despues de correr las pruebas
   end
   def test_simple
-   	
+   	assert_equal( (@fraccion1 + @fraccion2).to_s,"3/4") 
+	assert_equal( (@fraccion1 - @fraccion2).to_s, "1/4")
+	assert_equal( (@fraccion2 - @fraccion1).to_s, "-1/4")
+	assert_equal( (@fraccion1 / @fraccion2).to_s, "2")
+	assert_equal( (@fraccion2 / @fraccion1).to_s, "1/2")
+	assert_equal( (@fraccion2 * @fraccion3).to_s, "1/2")
+	assert_equal( (@fraccion3 * @fraccion2).to_s, "1/2")
   end
   def test_type_check
  
